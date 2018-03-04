77843a1bba0cc04bba203d96b7853cd0b2707ad4
Adam Varco <adamvarco@live.com>
2018-03-04 18:03:00 -0500

    Update ReverseFile.cpp

--- ReverseFile.cpp
+++ ReverseFile.cpp
@@ -3,10 +3,12 @@
 #include <string>
 #include <vector>
 int main(int argc, char** argv) {
-    if (argc < 2)
+    if (argc != 3)
         return(EXIT_FAILURE);
      std::vector<std::string> lines_in_reverse;
      std::ifstream in(argv[1]);
+     std::ofstream myfile;
+     myfile.open(argv[2]);
 
      if (!in)
        exit(EXIT_FAILURE);
@@ -19,6 +21,10 @@ int main(int argc, char** argv) {
         }
         in.close();
        }
-for (std::vector<std::string>::const_iterator i = lines_in_reverse.begin(); i != lines_in_reverse.end(); ++i)
+for (std::vector<std::string>::const_iterator i = lines_in_reverse.begin(); i != lines_in_reverse.end(); ++i){
     std::cout << *i <<"\n";
+    myfile << *i << "\n";
+ }
+    myfile.close();
+    
 }
